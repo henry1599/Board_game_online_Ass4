4d893bdbb35beee5b9b05f9af943ba0ad8cd0487
trungnc3 <trungnc3@vng.com.vn>
2022-11-16 17:09:51 +0700

    init server

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+
+  ws.on('close', function close() {
+    console.log(ws.id," close")
+  })
+});

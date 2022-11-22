b6ec27a26c9aea01d246a79527ff190a2e885c83
Toàn. Phan Đinh Minh <pdminhtoan@gmail.com>
2022-11-22 17:08:48 +0700

    Update Movement using Minimax

--- Custom Boardgame online/Assets/Scripts/Input/AI/MinimaxInput.cs
+++ Custom Boardgame online/Assets/Scripts/Input/AI/MinimaxInput.cs
@@ -15,93 +15,101 @@ public class MinimaxInput : AIInput
             CurrentCharacterBlock.Add(kv.Key, kv.Value.CurrentBlock);
         }
 
-        StartCoroutine(FindNextMove());
+        StartCoroutine(MinimaxRoot(3));
     }
 
-    private IEnumerator FindNextMove()
-    {
-        var currentblocksData = new BlocksData(LevelManager.Instance.blocksData);
-        var startBlock = CurrentCharacterBlock[character.Id];
-        Block bestTargetBlock = null;
-        int bestEvaluation = -100;
-        List<int> evaluationList = new List<int>();
-        foreach (Vector2Int moveableBlock in character.MoveableBlocks)
-        {
-            yield return null;
-            Block targetBlock = LevelManager.Instance.GetBlock(moveableBlock);
-            List<Vector2Int> movePath = Utils.GetMovePath(startBlock, targetBlock);
-            var newBlocksData = Utils.GetNewBlocksData(currentblocksData, character.Id, movePath);
-            int evaluation = -Search(newBlocksData, character.Id, 2);
-            // int evaluation = Utils.GetReward(newBlocksData, character.Id);
-            if (evaluation >= bestEvaluation)
-            {
-                bestTargetBlock = targetBlock;
-                bestEvaluation = evaluation;
-            }
-            evaluationList.Add(evaluation);
-        }
-        // Debug.Log($"x: {bestTargetBlock.data.Idx.x}, y: {bestTargetBlock.data.Idx.y} -> evaluation: {bestEvaluation}");
-        // string debug = "";
-        // foreach (int eval in evaluationList)
-        // {
-        //     debug += eval + " ";
-        // }
-        // Debug.Log($"Evaluations: {debug}");
-        OnGetInput?.Invoke(character, bestTargetBlock);
-    }
-
-    private int Search(BlocksData currentblocksData, string charId, int depth = 0, bool isMaximizeCharacter = false)
+    private int MinimaxSearch(int depth, BlocksData data, string charId, int alpha, int beta, bool isMaximizeCharacter = false)
     {
         if (depth == 0)
         {
-            int flag = isMaximizeCharacter ? 1 : -1;
-            return Utils.GetReward(currentblocksData, charId) * flag;
+            return Utils.GetReward(data, character.Id, true);
         }
-
         int bestEvaluation;
 
-        string nextCharId = LevelManager.Instance.GetNextCharacter(charId).Id;
-        Block startBlock = CurrentCharacterBlock[nextCharId];
+        Block startBlock = CurrentCharacterBlock[charId];
         List<Vector2Int> moveableBlocks = Utils.GetMoveableBlocks(startBlock.data);
+
+        string nextCharId = LevelManager.Instance.GetNextCharacter(charId).Id;
         if (isMaximizeCharacter)
         {
             bestEvaluation = -1000;
-            foreach (Vector2Int moveableBlock in character.MoveableBlocks)
+            foreach (Vector2Int moveableBlock in moveableBlocks)
             {
                 // Forward
                 Block targetBlock = LevelManager.Instance.GetBlock(moveableBlock);
-                CurrentCharacterBlock[nextCharId] = targetBlock;
+                CurrentCharacterBlock[charId] = targetBlock;
                 List<Vector2Int> movePath = Utils.GetMovePath(startBlock, targetBlock);
 
                 // Evaluate
-                var newBlocksData = Utils.GetNewBlocksData(currentblocksData, nextCharId, movePath);
-                int evaluation = Search(newBlocksData, nextCharId, depth - 1);
+                var newBlocksData = Utils.GetNewBlocksData(data, charId, movePath);
+                int evaluation = MinimaxSearch(depth - 1, newBlocksData, nextCharId, alpha, bestEvaluation, !isMaximizeCharacter);
                 bestEvaluation = Mathf.Max(bestEvaluation, evaluation);
 
                 // Back
-                CurrentCharacterBlock[nextCharId] = startBlock;
+                CurrentCharacterBlock[charId] = startBlock;
+
+                // Cut-off
+                alpha = Mathf.Max(alpha, bestEvaluation);
+                if (beta <= alpha)
+                    return bestEvaluation;
             }
         }
         else
         {
             bestEvaluation = 1000;
-            foreach (Vector2Int moveableBlock in character.MoveableBlocks)
+            foreach (Vector2Int moveableBlock in moveableBlocks)
             {
                 // Forward
                 Block targetBlock = LevelManager.Instance.GetBlock(moveableBlock);
-                CurrentCharacterBlock[nextCharId] = targetBlock;
+                CurrentCharacterBlock[charId] = targetBlock;
                 List<Vector2Int> movePath = Utils.GetMovePath(startBlock, targetBlock);
 
                 // Evaluate
-                var newBlocksData = Utils.GetNewBlocksData(currentblocksData, nextCharId, movePath);
-                int evaluation = Search(newBlocksData, nextCharId, depth - 1);
+                var newBlocksData = Utils.GetNewBlocksData(data, charId, movePath);
+                int evaluation = MinimaxSearch(depth - 1, newBlocksData, nextCharId, alpha, bestEvaluation, !isMaximizeCharacter);
                 bestEvaluation = Mathf.Min(bestEvaluation, evaluation);
 
                 // Back
-                CurrentCharacterBlock[nextCharId] = startBlock;
+                CurrentCharacterBlock[charId] = startBlock;
+
+                // Cut-off
+                beta = Mathf.Min(beta, bestEvaluation);
+                if (beta <= alpha)
+                    return bestEvaluation;
             }
         }
 
         return bestEvaluation;
+    }
+    private IEnumerator MinimaxRoot(int depth)
+    {
+        if (depth < 1)
+            depth = 1;
+
+        Block bestTargetBlock = null;
+        int bestEvaluation = -1000;
+        var currentblocksData = new BlocksData(LevelManager.Instance.blocksData);
+        var startBlock = CurrentCharacterBlock[character.Id];
+
+        foreach (Vector2Int moveableBlock in character.MoveableBlocks)
+        {
+            yield return null;
+            // forward 
+            Block targetBlock = LevelManager.Instance.GetBlock(moveableBlock);
+            List<Vector2Int> movePath = Utils.GetMovePath(startBlock, targetBlock);
+            var newBlocksData = Utils.GetNewBlocksData(currentblocksData, character.Id, movePath);
+
+            // compare
+            int evaluation = MinimaxSearch(depth - 1, newBlocksData, character.Id, -1000, 1000);
+            if (evaluation >= bestEvaluation)
+            {
+                bestTargetBlock = targetBlock;
+                bestEvaluation = evaluation;
+            }
+
+            // backward
+        }
+
+        OnGetInput?.Invoke(character, bestTargetBlock);
     }
 }
